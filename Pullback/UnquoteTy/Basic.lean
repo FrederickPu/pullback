2d97c5f77fc18d60243dcac7993370faddc2b0ab
FrederickPu <pufrederick51@gmail.com>
2025-11-23 00:15:22 -0500

    sketched do notation for quoted monad MonadTy

--- Pullback/UnquoteTy/Basic.lean
+++ Pullback/UnquoteTy/Basic.lean
@@ -1,3 +1,6 @@
+/-
+  TODO :: emulate Monad where proof carrying logic is moved into a seperate typeclass called LawfulUnquoteTy
+-/
 class UnquoteTy (Ty : Type) where
   -- `syntax T` returns Type describing the syntax of quoted type `T : Ty`
   «syntax» : Ty → Type
@@ -5,19 +8,69 @@ class UnquoteTy (Ty : Type) where
   interpret : Ty → Type
   unquote  (T : Ty) (t : «syntax» T) : interpret T
   imp (α β : Ty) : Ty
-  interpret_imp (α β : Ty) : interpret (imp α β) = interpret α → interpret β
+  interpret_imp (α β : Ty) : interpret (imp α β) = (interpret α → interpret β)
   unit : Ty
   hUnit : interpret unit = Unit
 
-
-infixr:80 "→" => UnquoteTy.imp
+infixr:30 "→" => UnquoteTy.imp
 open UnquoteTy
+
+-- can be defined using map and `UnquoteTy.interpret_imp`
+def UnquoteTy.app {Ty : Type} [UnquoteTy Ty] (α β : Ty) : «syntax» (α → β) → «syntax» α → «syntax» β := sorry
+
+/-
+  TODO should add this to `push_cast` and `norm_cast` tactic somehow
+-/
+example {Ty : Type} [UnquoteTy Ty] {α β : Ty} : «syntax» (α → β) = («syntax» α) → («syntax» β) := sorry
 
 class MonadTy {Ty : Type} [UnquoteTy Ty] (m : Ty → Ty) where
   map {α β : Ty} : «syntax» (((α → β) → (m α)) → (m β))
   mapConst {α β : Ty} : «syntax» (α → m β → m α)
   pure {α : Ty} : «syntax» (α → (m α))
   seq {α β : Ty} : «syntax» ((m (imp α β)) → (unit → (m α)) → (m β))
-  seqLeft {α β : Ty} : «syntax» ((m α) → (unit → (m β)) → (m α))
-  seqRight {α β : Ty} : «syntax» ((m α) → (unit → (m β)) → (m β))
+  seqLeft {α β : Ty} : «syntax» ((m α) → (unit : Ty) → (m β) → (m α))
+  seqRight {α β : Ty} : «syntax» ((m α) → (unit : Ty) → (m β) → (m β))
   bind {α β : Ty} : «syntax» ((m α) → (α → (m β)) → (m β))
+
+
+-- A simple object language with just base types and functions
+inductive SimpleTy where
+  | nat : SimpleTy
+  | bool : SimpleTy
+  | unit : SimpleTy
+  | arrow : SimpleTy → SimpleTy → SimpleTy
+
+-- Define syntax interpretation recursively
+def SimpleTy.syntax : SimpleTy → Type
+  | .nat => Nat
+  | .bool => Bool
+  | unit => Unit
+  | .arrow α β => SimpleTy.syntax α → SimpleTy.syntax β
+
+-- Define semantic interpretation recursively
+def SimpleTy.interpret : SimpleTy → Type
+  | .nat => Nat
+  | .bool => Bool
+  | unit => Unit
+  | .arrow α β => interpret α → interpret β
+
+theorem crux : SimpleTy.syntax = SimpleTy.interpret := by
+  ext T
+  induction T
+  · rfl
+  · rfl
+  · rfl
+  · expose_names
+    simp [SimpleTy.syntax, SimpleTy.interpret, a_ih, a_ih_1]
+
+-- UnquoteTy instance for our simple type language
+instance : UnquoteTy SimpleTy where
+  «syntax» := SimpleTy.syntax
+  interpret := SimpleTy.interpret
+  unquote := fun T t => cast (by simp [crux]) t
+  imp := SimpleTy.arrow
+  interpret_imp := fun α β => rfl
+  unit := SimpleTy.unit
+  hUnit := rfl
+
+instance : MonadTy (id : SimpleTy → SimpleTy) := sorry
---
+++ Pullback/UnquoteTy/Do.lean
@@ -0,0 +1,50 @@
+import Pullback.UnquoteTy.Basic
+/-! # Basic `do` notation -/
+
+open Lean
+
+declare_syntax_cat stmt
+syntax "do'" stmt : term
+
+-- Prevent `if ...` from being parsed as a term
+syntax (priority := low) term : stmt
+syntax "let" ident "←" stmt:1 ";" stmt : stmt
+macro "{" s:stmt "}" : stmt => `(stmt| $s)
+
+/-
+  Remark: we annotate `macro`s and `macro_rules` with their corresponding
+  translation/abbreviation id from the paper.
+-/
+
+syntax "d!" stmt : term  -- corresponds to `D(s)`
+
+macro_rules
+  | `(do' $s) => `(d! $s)  -- (1)
+
+-- helper function; see usage below
+def expandStmt (s : TSyntax `stmt) : MacroM (TSyntax `stmt) := do
+  let s' ← expandMacros s
+  if s == s' then
+    Macro.throwUnsupported
+  else
+    -- There is no static guarantee that `expandMacros` stays in the `stmt` category,
+    -- but it is true for all our macros
+    return TSyntax.mk s'
+
+#check UnquoteTy.app
+macro_rules
+  | `(d! $e:term) => `($e)                                       -- (D1)
+  | `(d! let $x ← $s; $s') => `(UnquoteTy.app (UnquoteTy.app MonadTy.bind (d! $s)) (fun $x => (d! $s')))  -- (D2)
+  | `(d! $s) => do
+    -- fallback rule: try to expand abbreviation
+    let s' ← expandStmt s
+    `(d! $s')
+
+macro "let" x:ident ":=" e:term ";" s:stmt : stmt => `(stmt| let $x ← MonadTy.pure $e; $s)  -- (A1)
+-- priority `0` prevents `;` from being used in trailing contexts without braces (see e.g. `:1` above)
+macro:0 s₁:stmt ";" s₂:stmt : stmt => `(stmt| let x ← $s₁; $s₂)                     -- (A2)
+
+open UnquoteTy MonadTy
+def test : «syntax» (id SimpleTy.nat) := do'
+  let x := 10;
+  return x
