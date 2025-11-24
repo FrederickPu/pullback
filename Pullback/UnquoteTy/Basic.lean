2c7d270bc326e706738118974ebefec874194028
FrederickPu <pufrederick51@gmail.com>
2025-11-24 15:46:13 -0500

    sketched sequente logic based unquote

--- Pullback/UnquoteTy/Basic.lean
+++ Pullback/UnquoteTy/Basic.lean
@@ -2,6 +2,8 @@
   TODO :: emulate Monad where proof carrying logic is moved into a seperate typeclass called LawfulUnquoteTy
 
   note that UnquoteTy's unquoting operation doesn't support dependent types so can only model simple type theory.
+
+  goal: no explicit judgements (all judgements done implcitly through ambient type theory / lean's unification system)
 -/
 class UnquoteTy (Ty : Type) where
   -- `syntax T` returns Type describing the syntax of quoted type `T : Ty`
@@ -16,7 +18,100 @@ class UnquoteTy (Ty : Type) where
   -/
   app {α β : Ty} : «syntax» (imp α β) → «syntax» α → «syntax» β
 
+#check Bind
+class Unquote (Ty : Type) (rules : List (List Ty × Ty)) where
+  -- `interpret t` returns Type describing the smenatics of quoted type `t : Ty`
+  interpret : List Ty → Ty → Type
+  unquote_intro (ruleIdx : Fin rules.length) : interpret (rules.get ruleIdx).1 (rules.get ruleIdx).2
+  unquote_cut (Γ' Γ : List Ty) (T α : Ty) (a : interpret Γ' α) (t : interpret (α::Γ) T) : interpret (Γ' ++ Γ) T
+
+inductive WompWomp (Ty : Type) (rules : List (List Ty × Ty)) : (Γ : List Ty) → (T : Ty) → Type where
+| intro (ruleIdx : Fin rules.length) : WompWomp Ty rules (rules.get ruleIdx).1 (rules.get ruleIdx).2
+| cut (Γ' Γ : List Ty) (T α : Ty) (a : WompWomp Ty rules Γ' α) (t : WompWomp Ty rules (α::Γ) T) : WompWomp Ty rules (Γ' ++ Γ) T
+
+inductive WompWompLam (Ty : Type) (rules : List (List Ty × Ty)) : (Γ : List Ty) → (T : Ty) → Type where
+| intro (ruleIdx : Fin rules.length) : WompWompLam Ty rules (rules.get ruleIdx).1 (rules.get ruleIdx).2
+| cut (Γ' Γ : List Ty) (T α : Ty) (a : WompWompLam Ty rules Γ' α) (t : WompWompLam Ty rules (α::Γ) T) : WompWompLam Ty rules (Γ' ++ Γ) T
+| var (Γ : List Ty) (i : Fin Γ.length) : WompWompLam Ty rules Γ (Γ.get i)
+
+
+def WompWomp.unquote
+  {Ty : Type} {rules : List (List Ty × Ty)}
+  [U : Unquote Ty rules]
+  : (Γ : List Ty) → (T : Ty) → WompWomp Ty rules Γ T →
+      Unquote.interpret rules Γ T
+| _, _, WompWomp.intro ruleIdx => U.unquote_intro ruleIdx
+| _, _, WompWomp.cut Γ' Γ T α a t =>
+  let ua := WompWomp.unquote Γ' α a
+  let ut := WompWomp.unquote (α :: Γ) T t
+  U.unquote_cut Γ' Γ T α ua ut
+
+open WompWompLam
+
+def nestedVarLam
+  {Ty : Type} {rules : List (List Ty × Ty)} [Unquote Ty rules]
+  : (Γ : List Ty) → (i : Fin Γ.length) →
+    Unquote.interpret rules Γ (Γ.get i) :=
+  sorry
+
+
+def WompWompLam.unquote
+  {Ty : Type} {rules : List (List Ty × Ty)}
+  [U : Unquote Ty rules]
+  : (Γ : List Ty) → (T : Ty) → WompWompLam Ty rules Γ T →
+      Unquote.interpret rules Γ T
+| _, _, WompWompLam.intro ruleIdx =>
+  U.unquote_intro ruleIdx
+| _, _, WompWompLam.cut Γ' Γ'' T α a t =>
+  let ua := WompWompLam.unquote Γ' α a
+  let ut := WompWompLam.unquote (α :: Γ'') T t
+  U.unquote_cut Γ' Γ'' T α ua ut
+| _, _, WompWompLam.var Γ i => nestedVarLam Γ i
+
+open UnquoteTy
+
 infixr:30 "→" => UnquoteTy.imp
+
+
+inductive MyLang where
+| A : MyLang
+| Arrow : MyLang -> MyLang -> MyLang
+
+def CounterExample.syntax : MyLang → Type
+| MyLang.A => Nat
+| MyLang.Arrow X Y => CounterExample.syntax X → CounterExample.syntax Y
+
+/-
+  Parametricity theorem
+-/
+example : ¬ ∀ [UnquoteTy MyLang] (f : («syntax» (imp MyLang.A MyLang.A : MyLang))), app f = id := by {
+  simp only [Classical.not_forall]
+  apply Exists.intro {
+    «syntax» := CounterExample.syntax
+    interpret := CounterExample.syntax,
+    unquote := fun x y => y
+    imp := fun x y => MyLang.Arrow x y
+    interpret_imp := by
+      intro α β
+      rfl
+    app := fun f x => f x
+  }
+  apply Exists.intro ((cast rfl) (fun x => 2 * x : Nat → Nat))
+  simp only [cast_eq]
+  intro h
+  have := congrFun h (1:Nat)
+  simp at this
+}
+
+def MyLang.syntax : MyLang → Type
+| MyLang.A => Nat
+| MyLang.Arrow MyLang.A MyLang.A => Unit
+| MyLang.Arrow X Y => MyLang.syntax X → MyLang.syntax Y
+
+def MyLang.interpret : MyLang → Type
+| MyLang.A => Nat
+| MyLang.Arrow X Y => MyLang.syntax X → MyLang.syntax Y
+
 open UnquoteTy
 
 notation:20 x "<||" y => UnquoteTy.app x y
@@ -83,3 +178,18 @@ instance : Coe Nat («syntax» SimpleTy.nat) :=
   ⟨fun x => x⟩
 
 def SimpleTy.idNat : «syntax» (SimpleTy.nat → id SimpleTy.nat) := id
+
+/-!
+Note why quoting is necessary/you cant just use paraemtric types. For example if you try to make a syntehtic where all functino are polynomial time eg (can only use succ) you will always be able to escape that theory by using lean's native recursors:
+
+```
+axiom P : Type → Type
+axiom succ : P ℕ → P ℕ
+
+noncomputable example (a : P ℕ) : P ℕ :=
+  Nat.rec a (fun n b => succ b) (1000 * 1000^2)
+```
+
+
+idea : make `Ty` where all function types are continuous, surjective, injective etc
+-/
