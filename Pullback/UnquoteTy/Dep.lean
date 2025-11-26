255f6532aa9defc42a52faa448c04993cbe410b1
FrederickPu <pufrederick51@gmail.com>
2025-11-25 20:12:20 -0500

    add sketch of dependently typed contexts

---
+++ Pullback/UnquoteTy/Dep.lean
@@ -0,0 +1,51 @@
+-- dependent contexts
+
+/-
+  dependently typed vector
+-/
+inductive DVec (α : Nat → Type) : Nat → Type
+  | nil : DVec α 0
+  | push {n : Nat} : DVec α n → α n → DVec α (n + 1)
+
+/--
+  The dependently typed vector whose first element is `head`, where `tail` is the rest of the vector.
+  Usually written `head ::: tail`.
+-/
+def DVec.cons {α : Nat → Type} : α 0 → DVec α n → DVec α (n + 1) := sorry
+
+@[inherit_doc] infixr:67 " ::: " => DVec.cons
+
+/-
+  dependently typed list
+-/
+inductive DList (α : Nat → Type) where
+| mk (n : Nat) : DVec α n → DList α
+
+def DList.get {α : Nat → Type} {n : Nat} (l : DList α) (i : Fin n) : α i := sorry
+
+def DList.get' {α : Nat → Type} {n : Nat} (l : DList α) (i : Fin n) : α n := sorry
+
+inductive DTerm (Ty : Type) (i : Nat) where
+| type (t : Ty)
+| var (i : Nat)
+| app (T₁ T₂ : Ty)
+
+def DTerm.shift {Ty : Type} {i : Nat} {j} : DTerm Ty i → DTerm Ty (i + j) := sorry
+
+instance {α : Nat → Type} : Append (DList α) := sorry
+
+theorem DList.cons_size {α : Nat → Type} (A B : DList α) : (A ++ B).1 = A.1 + B.1 := sorry
+
+/-
+  `(Γ : DContext Ty)` is a dependtly typed context over a quoted type universe `Ty`
+  a context is a list of terms where each term is either a type or a variable references one of the terms occuring before it in the context
+-/
+abbrev DContext (Ty : Type) := DList (fun i => DTerm Ty i)
+
+inductive WompWompLam (Ty : Type) (rules : List ((Γ : DContext Ty) × (DTerm Ty Γ.1))) : (Γ : DContext Ty) → (T : DTerm Ty Γ.1) → Type where
+| intro (ruleIdx : Fin rules.length) : WompWompLam Ty rules (rules.get ruleIdx).1 (rules.get ruleIdx).2
+| cut (Γ' Γ : DContext Ty) (α : Ty) (T : DTerm Ty Γ.1) (a : WompWompLam Ty rules Γ' (DTerm.type α)) : (t : WompWompLam Ty rules (⟨_, (DTerm.type α):::Γ.2⟩) T.shift) → WompWompLam Ty rules (Γ' ++ Γ) (cast (by {
+  congr
+  rw [DList.cons_size, Nat.add_comm]
+}) (T.shift (j := Γ'.1)))
+| var (Γ : DContext Ty) (i : Fin Γ.1) : WompWompLam Ty rules Γ (Γ.get' i)
