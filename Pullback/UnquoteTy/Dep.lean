aa43f9e310acfc35aacc3d34c662f732ce6faa43
FrederickPu <pufrederick51@gmail.com>
2025-11-25 22:08:36 -0500

    added unquoting to Dependent sequents

--- Pullback/UnquoteTy/Dep.lean
+++ Pullback/UnquoteTy/Dep.lean
@@ -51,3 +51,32 @@ inductive WompWompLam (Ty : Type) (rules : List ((Γ : DContext Ty) × (DTerm Ty
   rw [DList.cons_size, Nat.add_comm]
 }) (T.shift (j := Γ'.1)))
 | var (Γ : DContext Ty) (i : Fin Γ.1) : WompWompLam Ty rules Γ (Γ.get' i)
+
+
+class Unquote (Ty : Type) (rules : List ((Γ : DContext Ty) × DTerm Ty Γ.1)) where
+  -- `interpret t` returns Type describing the smenatics of quoted type `t : Ty`
+  interpret : (Γ : DContext Ty) → DTerm Ty Γ.1 → Type
+  unquote_intro (ruleIdx : Fin rules.length) : interpret (rules.get ruleIdx).1 (rules.get ruleIdx).2
+  unquote_cut (Γ' Γ : DContext Ty) (α : Ty) (T : DTerm Ty Γ.1) (a : interpret Γ' (DTerm.type α)) (t : interpret (DTerm.type α:::Γ) T.shift) : interpret (Γ' ++ Γ) (cast (by {
+  congr
+  rw [DList.cons_size, Nat.add_comm]
+}) (T.shift (j := Γ'.1)))
+
+def nestedVarLam
+  {Ty : Type} {rules : List ((Γ : DContext Ty) × DTerm Ty Γ.1)} [Unquote Ty rules]
+  : (Γ : DContext Ty) → (i : Fin Γ.1) →
+    Unquote.interpret rules Γ (Γ.get' i) :=
+  sorry
+
+def WompWompLam.unquote
+  {Ty : Type} {rules : List ((Γ : DContext Ty) × DTerm Ty Γ.1)}
+  [U : Unquote Ty rules]
+  : (Γ : DContext Ty) → (T : DTerm Ty Γ.1) → WompWompLam Ty rules Γ T →
+      Unquote.interpret rules Γ T
+| _, _, WompWompLam.intro ruleIdx =>
+  U.unquote_intro ruleIdx
+| _, _, WompWompLam.cut Γ' Γ'' α T a t =>
+  let ua := WompWompLam.unquote Γ' (DTerm.type α) a
+  let ut := WompWompLam.unquote (DTerm.type α ::: Γ'') T.shift t
+  U.unquote_cut Γ' Γ'' α T ua ut
+| _, _, WompWompLam.var Γ i => nestedVarLam Γ i
