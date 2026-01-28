fcee8687b93e14ec17ccf4dd9cd5f08d4a89ce14
FrederickPu <pufrederick51@gmail.com>
2026-01-28 01:20:36 -0500

    partially defined attention

--- Pullback/NN/Transformer.lean
+++ Pullback/NN/Transformer.lean
@@ -30,7 +30,24 @@ def biasNorm
 -/
 def attention (keys : Array (NDMatrix ℝ [d])) (query : NDMatrix ℝ [d]) : Array ℝ := keys.map (fun key => (key * query).sum)
 
--- /-
---   batches attention computation across all keys from the given token sequence
--- -/
--- def attentionSeqBatch (keys : NDMatrix ℝ [l, d]) (queries : NDMatrix ℝ [l, d]) : NDMatrix ℝ [l, l] := queries.transpose.matmul keys
+#check Matrix.mulᵣ
+#check Matrix.transpose
+#check Array.getElem?_append
+
+/-
+  batches attention computation across all keys from the given token sequence
+-/
+def attentionSeqBatch (keys : NDMatrix ℝ [l, d]) (queries : NDMatrix ℝ [l, d]) : NDMatrix ℝ [l, l] := Matrix.mulᵣ queries (Matrix.transpose keys)
+
+
+theorem attention_eq_get_attentionSeqBatch (keys queries : Array (NDMatrix ℝ [d])) (h : keys.size = queries.size):
+  ∀ i : Fin queries.size, ∀ j : Fin queries.size, (attention keys (queries[i]))[j]'(by simp [attention, h]) = (attentionSeqBatch (fun i : Fin queries.size => keys[i]) (queries[·])) i j := by
+  intro i j
+  simp [attention, attentionSeqBatch, Matrix.transpose, Matrix.mulᵣ, dotProduct, HMul.hMul, Mul.mul, NDMatrix.sum, Finset.sum, List.sum]
+  apply congr
+  rfl
+  rw [List.ofFn_eq_map]
+  simp only [List.map_inj_left, List.mem_finRange, forall_const]
+  intro idx
+  have : (keys[↑j] idx) *  (queries[↑i] idx) = (queries[↑i] idx)  * (keys[↑j] idx) := by ring
+  exact this
