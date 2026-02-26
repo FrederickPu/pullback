ae07e782091611ba6af3b68f5e72256b1df153b7
FrederickPu <pufrederick51@gmail.com>
2026-02-26 16:43:48 -0500

    back up NN changes

--- Pullback/NN/Basic.lean
+++ Pullback/NN/Basic.lean
@@ -8,7 +8,7 @@ def NDMatrix (α : Type u) : List Nat → Type u
 | [] => α
 | (a::l) => Fin a → NDMatrix α l
 
-def NDMatrix.map {α : Type u} (f : α → α) : {shape : List Nat} → NDMatrix α shape → NDMatrix α shape
+def NDMatrix.map {α : Type u} {β : Type v} (f : α → β) : {shape : List Nat} → NDMatrix α shape → NDMatrix β shape
 | [] => f
 | _::l => fun x => fun i => NDMatrix.map f (shape := l) (x i)
 
@@ -16,11 +16,11 @@ instance NDMatrix.instInhabited {α : Type u} [Inhabited α] : {shape : List Nat
   | [] => inferInstance
   | _ :: shape => letI := instInhabited (α := α) (shape := shape); inferInstance
 
-def NDMatrix.broadcastMapAux {α : Type u} {s₁ s₂ : List Nat} (f : NDMatrix α s₁ → NDMatrix α s₂) : (shapePrefix : List Nat) → NDMatrix α (shapePrefix ++ s₁) → NDMatrix α (shapePrefix ++ s₂)
+def NDMatrix.broadcastMapAux {α : Type u} {β : Type v} {s₁ s₂ : List Nat} (f : NDMatrix α s₁ → NDMatrix β s₂) : (shapePrefix : List Nat) → NDMatrix α (shapePrefix ++ s₁) → NDMatrix β (shapePrefix ++ s₂)
 | [] => f
 | _::l => fun x => fun i => NDMatrix.broadcastMapAux f l (x i)
 
-def NDMatrix.broadcastMap {α : Type u} {s₁ s₂ : List Nat} (f : NDMatrix α s₁ → NDMatrix α s₂) {shape : List Nat} (hShape : shape.drop (shape.length - s₁.length) = s₁ := by simp) : NDMatrix α shape → NDMatrix α (shape.take (shape.length - s₁.length) ++ s₂) :=
+def NDMatrix.broadcastMap {α : Type u} {β : Type v} {s₁ s₂ : List Nat} (f : NDMatrix α s₁ → NDMatrix β s₂) {shape : List Nat} (hShape : shape.drop (shape.length - s₁.length) = s₁ := by simp) : NDMatrix α shape → NDMatrix β (shape.take (shape.length - s₁.length) ++ s₂) :=
   cast (by {
     have : shape = shape.take (shape.length - s₁.length) ++ shape.drop (shape.length - s₁.length) :=
       Eq.symm (List.take_append_drop (shape.length - s₁.length) shape)
@@ -28,14 +28,14 @@ def NDMatrix.broadcastMap {α : Type u} {s₁ s₂ : List Nat} (f : NDMatrix α 
     grind
   }) (NDMatrix.broadcastMapAux f (shape.take (shape.length - s₁.length)))
 
-def NDMatrix.broadcastMap? {α : Type u} {s₁ s₂ : List Nat} (f : NDMatrix α s₁ → NDMatrix α s₂) : {shape : List Nat} → NDMatrix α shape → Option (NDMatrix α (shape.take (shape.length - s₁.length) ++ s₂)) :=
+def NDMatrix.broadcastMap? {α : Type u} {β : Type v} {s₁ s₂ : List Nat} (f : NDMatrix α s₁ → NDMatrix β s₂) : {shape : List Nat} → NDMatrix α shape → Option (NDMatrix β (shape.take (shape.length - s₁.length) ++ s₂)) :=
   fun {shape} x =>
     if h : shape.drop (shape.length - s₁.length) = s₁ then
       some <| NDMatrix.broadcastMap f h x
     else
       none
 
-def NDMatrix.broadcastMap! {α : Type u} [Inhabited α] {s₁ s₂ : List Nat} (f : NDMatrix α s₁ → NDMatrix α s₂) : {shape : List Nat} → NDMatrix α shape → NDMatrix α (shape.take (shape.length - s₁.length) ++ s₂) :=
+def NDMatrix.broadcastMap! {α : Type u} {β : Type v} [Inhabited β] {s₁ s₂ : List Nat} (f : NDMatrix α s₁ → NDMatrix β s₂) : {shape : List Nat} → NDMatrix α shape → NDMatrix β (shape.take (shape.length - s₁.length) ++ s₂) :=
   fun {shape} =>
     if h : shape.drop (shape.length - s₁.length) = s₁ then
       NDMatrix.broadcastMap f h
--- Pullback/NN/Transformer.lean
+++ Pullback/NN/Transformer.lean
@@ -24,11 +24,25 @@ def biasNorm
     fun i =>
       x i / (rms (x - β)) * Real.exp (γ i)
 
+def NDMatrix.softmax : NDMatrix EReal [d] → NDMatrix ℝ [d] := sorry
+
+#check Array.mapFinIdx
+open EReal ENNReal
+#check EReal
+
+def List.softmax (l : List EReal) : List ℝ := List.ofFn (NDMatrix.softmax l.get)
+
+def Array.softmax (l : Array EReal) : Array ℝ := l.toList.softmax.toArray
+
+theorem womp (vals : Array ℝ) (i : Fin vals.size) : ∀ j, j ≤ i → (NDMatrix.softmax ((vals.mapFinIdx (fun j (x:ℝ) h => (if j ≤ i then x else ∞ : EReal))).toList.get) ⟨j, by {
+  sorry
+}⟩) = (vals.map (fun x : ℝ => (x : EReal))).softmax[j]' sorry := sorry
+
 /-
  attention for a single query
  TODO :: add softmax and multiplication by values
 -/
-def attention (keys : Array (NDMatrix ℝ [d])) (query : NDMatrix ℝ [d]) : Array ℝ := keys.map (fun key => (key * query).sum)
+def attention (keys : Array (NDMatrix ℝ [d])) (query : NDMatrix ℝ [d]) : Array ℝ := Array.softmax (keys.map (fun key => (key * query).sum))
 
 #check Matrix.mulᵣ
 #check Matrix.transpose
@@ -37,17 +51,13 @@ def attention (keys : Array (NDMatrix ℝ [d])) (query : NDMatrix ℝ [d]) : Arr
 /-
   batches attention computation across all keys from the given token sequence
 -/
-def attentionSeqBatch (keys : NDMatrix ℝ [l, d]) (queries : NDMatrix ℝ [l, d]) : NDMatrix ℝ [l, l] := Matrix.mulᵣ queries (Matrix.transpose keys)
+def attentionSeqBatch (keys : NDMatrix ℝ [l, d]) (queries : NDMatrix ℝ [l, d]) : NDMatrix ℝ [l, l] := (NDMatrix.broadcastMap (NDMatrix.softmax)) (NDMatrix.map (fun x : ℝ => (x : EReal)) (NDMatrix.instMul.mul (NDMatrix.triu l) (Matrix.mulᵣ queries (Matrix.transpose keys))))
 
 
 theorem attention_eq_get_attentionSeqBatch (keys queries : Array (NDMatrix ℝ [d])) (h : keys.size = queries.size):
-  ∀ i : Fin queries.size, ∀ j : Fin queries.size, (attention keys (queries[i]))[j]'(by simp [attention, h]) = (attentionSeqBatch (fun i : Fin queries.size => keys[i]) (queries[·])) i j := by
-  intro i j
-  simp [attention, attentionSeqBatch, Matrix.transpose, Matrix.mulᵣ, dotProduct, HMul.hMul, Mul.mul, NDMatrix.sum, Finset.sum, List.sum]
-  apply congr
-  rfl
-  rw [List.ofFn_eq_map]
-  simp only [List.map_inj_left, List.mem_finRange, forall_const]
-  intro idx
-  have : (keys[↑j] idx) *  (queries[↑i] idx) = (queries[↑i] idx)  * (keys[↑j] idx) := by ring
-  exact this
+  ∀ i : Fin queries.size, ∀ j : Fin queries.size, (hij : j ≤ i) → (attention (keys.take (i + 1)) (queries[i]))[j]'(by simp [attention, h, hij, Array.softmax, List.softmax]) = (attentionSeqBatch (fun i : Fin queries.size => keys[i]) (queries[·])) i j := by
+  intro i j hij
+  simp [attention, attentionSeqBatch, NDMatrix.sum, Matrix.mulᵣ, dotProduct, Finset.sum, List.ofFn_eq_map, NDMatrix.broadcastMap, NDMatrix.broadcastMapAux]
+  unfold NDMatrix.triu
+  simp [Mul.mul, NDMatrix.entrywise, Array.softmax]
+  sorry
